5a65950c1ab7de25715dfd32950e028df3aec1b0
Zzzxb <idearfree@outlook.com>
2023-04-07 19:06:42 +0800

    update style from login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import {AppComponent} from './component';
 import {Login} from "./login/login";
 import {Home} from "./home/home";
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
     declarations: [
         AppComponent,
@@ -14,6 +20,13 @@ import {Home} from "./home/home";
     imports: [
         BrowserModule,
         AppRoutingModule,
+        BrowserAnimationsModule,
+
+        // material 组件
+        MatTabsModule,
+        MatSidenavModule,
+        MatListModule,
+        MatButtonModule
     ],
     providers: [],
     bootstrap: [AppComponent]
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,5 +1,6 @@
 import {Component, Injectable} from '@angular/core';
 
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.html',
@@ -8,4 +9,43 @@ import {Component, Injectable} from '@angular/core';
 
 @Injectable( {providedIn: 'root'} )
 export class Home {
+    navNum = 0;
+
+    navList: NavManage[] = [
+        new NavManage('Toolkit', '#')
+            .appendSide('首页', '#'),
+        new NavManage('Document', '#')
+            .appendSide('首页', '#')
+            .appendSide('视频短信文档', '#')
+    ];
+
+    switchNav(num: number) {
+        this.navNum = num;
+    }
+
+    showSideNav() {
+        return this.navList[this.navNum].sn
+    }
+}
+class NavManage {
+    tm: TagManger;
+    sn: TagManger[] = []
+
+    constructor(tag: string, jumpUrl: string) {
+        this.tm = new TagManger(tag, jumpUrl);
+    }
+
+    appendSide(tag: string, jumpUrl: string) {
+        this.sn.push(new TagManger(tag, jumpUrl));
+        return this;
+    }
+}
+
+class TagManger {
+    tag: string = ''
+    jumpUrl: string = ''
+    constructor(tag: string, jumpUrl: string) {
+        this.tag = tag;
+        this.jumpUrl = jumpUrl;
+    }
 }
